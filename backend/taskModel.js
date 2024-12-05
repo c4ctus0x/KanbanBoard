da095494594500010cbc174a17986f98c3c94226
c4ctus0x <163348838+c4ctus0x@users.noreply.github.com>
2024-12-05 15:19:29 +0200

    improve code readability

--- backend/taskModel.js
+++ backend/taskModel.js
@@ -1,21 +1,25 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const { Schema } = mongoose; // Destructuring for a cleaner approach
 
+// Defining the task schema
 const taskSchema = new Schema({
   title: {
     type: String,
-    required: true
+    required: true,  // Indicating title is a must-have field
   },
   description: {
     type: String,
-    required: false
+    required: false, // Description is optional
   },
   status: {
     type: String,
-    default: 'To Do',
-    enum: ['To Do', 'In Progress', 'Done']
-  }
+    default: 'To Do', // Default status is 'To Do'
+    enum: ['To Do', 'In Progress', 'Done'], // Enum to ensure status is one of the predefined values
+  },
 });
 
+// Compiling our schema into a model
 const Task = mongoose.model('Task', taskSchema);
+
+// Exporting the Task model for use in other parts of the app
 module.exports = Task;
